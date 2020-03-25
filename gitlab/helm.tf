d3240384b580bbe097acb859c9c2196cf0c7576d
Mate Lang <mate.lang@connatix.com>
2020-03-25 14:33:25 +0200

    Added NodeSelectors

--- gitlab/helm.tf
+++ gitlab/helm.tf
@@ -43,34 +43,42 @@ locals {
     "gitlab.gitaly.tolerations[0].key"    = var.k8s_toleration_label.0.key
     "gitlab.gitaly.tolerations[0].value"  = var.k8s_toleration_label.0.value
     "gitlab.gitaly.tolerations[0].effect" = "NoSchedule"
+    "gitlab.gitaly.nodeSelector[0]" = "${var.k8s_toleration_label.0.key}=${var.k8s_toleration_label.0.value}"
 
     "gitlab.gitlab-exporter.tolerations[0].key"    = var.k8s_toleration_label.0.key
     "gitlab.gitlab-exporter.tolerations[0].value"  = var.k8s_toleration_label.0.value
     "gitlab.gitlab-exporter.tolerations[0].effect" = "NoSchedule"
+    "gitlab.gitlab-exporter.nodeSelector[0]" = "${var.k8s_toleration_label.0.key}=${var.k8s_toleration_label.0.value}"
 
     "gitlab.gitlab-runner.tolerations[0].key"    = var.k8s_toleration_label.0.key
     "gitlab.gitlab-runner.tolerations[0].value"  = var.k8s_toleration_label.0.value
     "gitlab.gitlab-runner.tolerations[0].effect" = "NoSchedule"
+    "gitlab.gitlab-runner.nodeSelector[0]" = "${var.k8s_toleration_label.0.key}=${var.k8s_toleration_label.0.value}"
 
     "gitlab.gitlab-shell.tolerations[0].key"    = var.k8s_toleration_label.0.key
     "gitlab.gitlab-shell.tolerations[0].value"  = var.k8s_toleration_label.0.value
     "gitlab.gitlab-shell.tolerations[0].effect" = "NoSchedule"
+    "gitlab.gitlab-shell.nodeSelector[0]" = "${var.k8s_toleration_label.0.key}=${var.k8s_toleration_label.0.value}"
 
     "gitlab.migrations.tolerations[0].key"    = var.k8s_toleration_label.0.key
     "gitlab.migrations.tolerations[0].value"  = var.k8s_toleration_label.0.value
     "gitlab.migrations.tolerations[0].effect" = "NoSchedule"
+    "gitlab.migrations.nodeSelector[0]" = "${var.k8s_toleration_label.0.key}=${var.k8s_toleration_label.0.value}"
 
     "gitlab.sidekiq.tolerations[0].key"    = var.k8s_toleration_label.0.key
     "gitlab.sidekiq.tolerations[0].value"  = var.k8s_toleration_label.0.value
     "gitlab.sidekiq.tolerations[0].effect" = "NoSchedule"
+    "gitlab.sidekiq.nodeSelector[0]" = "${var.k8s_toleration_label.0.key}=${var.k8s_toleration_label.0.value}"
 
     "gitlab.unicorn.tolerations[0].key"    = var.k8s_toleration_label.0.key
     "gitlab.unicorn.tolerations[0].value"  = var.k8s_toleration_label.0.value
     "gitlab.unicorn.tolerations[0].effect" = "NoSchedule"
+    "gitlab.unicorn.nodeSelector[0]" = "${var.k8s_toleration_label.0.key}=${var.k8s_toleration_label.0.value}"
 
     "shared-secrets.tolerations[0].key"    = var.k8s_toleration_label.0.key
     "shared-secrets.tolerations[0].value"  = var.k8s_toleration_label.0.value
     "shared-secrets.tolerations[0].effect" = "NoSchedule"
+    "shared-secrets.nodeSelector[0]" = "${var.k8s_toleration_label.0.key}=${var.k8s_toleration_label.0.value}"
   }
 
   # External Database
@@ -175,6 +183,7 @@ locals {
     "tolerations[0].key"    = var.ci_k8s_toleration_label.0.key
     "tolerations[0].value"  = var.ci_k8s_toleration_label.0.value
     "tolerations[0].effect" = "NoSchedule"
+    "nodeSelector[0]" = "${var.ci_k8s_toleration_label.0.key}=${var.ci_k8s_toleration_label.0.value}"
   }
 }
 
